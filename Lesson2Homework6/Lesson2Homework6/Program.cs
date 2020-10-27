60f971e01cd67d344a357a4f20b606554bab6444
unknown <rostykvoznyak@gmail.com>
2020-10-27 23:43:01 +0200

    Lesson2Homework6

---
+++ Lesson2Homework6/Lesson2Homework6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson2Homework6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int uberflu?= 9; //NO
+            int uberflu? = 0;//NO
+            int _Identifier = 5;//Yes
+            int \ u006fIdentifier = 0;//NO
+            int &myVar = 4;//NO
+            int myVariab1le = 6;//Yes
+        }
+    }
+}
